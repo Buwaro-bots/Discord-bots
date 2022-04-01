779f04ade55486772877cc5b12f4c9b4abbce50c
Buwaro-bots <superbenj@gmail.com>
2022-04-01 23:04:18 +0200

    DnG : Commande aide, table de type et PC.
    Et ajout d'un fichier pour des strings avec des liens privés.

--- Commandes/dng.js
+++ Commandes/dng.js
@@ -1,9 +1,80 @@
+const { verifierNaN } = require("./outils.js");
 const outils = require("./outils.js");
+const roll = require('./roll.js');
+let strings = require('../Données/textes-privés.json');
 
 exports.dng = function(client, message, args, envoyerPM, idMJ){
 
+    if (["aide", "help", "commandes", "commande"].includes(args[0])){
+        let espaces = "                ";
+        let botReply = "." + 
+        espaces + "**Liens utiles**\r\n" +
+        strings["DnG-Help"] +
+        " \r\n" +
+        espaces + "**Commandes**\r\n" +
+        "**;dng table** pour la table des types.\r\n" +
+        "**;dng *stat*** pour un jet normal, la stat doit normalement être entre 1 et 5.\r\n" +
+        "**;dng pc *stat* *+modificateur*** pour un jet de puissance cachée," +
+        " la stat correspond à l'échelon entre 1 et 20. La jauge a 4 points de plus que le niveau de l'échelon. \r\n" +
+        " \r\n" +
+        espaces + "**Jets normaux**\r\n" +
+        "Le premier lancer correspond au jet de caractéristique, le dé lancer correspond à votre stat. Pour une action normale, le jet est une réussite si le jet est supérieur à 3. (soit 4 ou plus).\r\n" +
+        "Le second lancer correspond au jet de crtitique, plus le lancer est haut, plus la réussite ou l'échec est importante, en général un 19 ou un 20 vaut une réussite ou échec critique." +
+        " Si un échec a un petit lancer de critique, il peut devenir une réussite si vous avez un avantage. Avec un avantage, un échec devient une réussite si le lancer de critique est entre 1 et 4. L'inverse est aussi vraie pour les petites réussites.\r\n";
 
-    let est_PC = false;
+
+        outils.envoyerMessage(client, botReply, message, envoyerPM, idMJ);
+        return;
+    }
+    if (args[0] === "table" || args[0] === "types"){
+        outils.envoyerMessage(client, "https://cdn.discordapp.com/attachments/730133304237359157/958476687090262066/unknown.png", message, envoyerPM, idMJ);
+        return;
+    }
+    if (args[0] == "pc" && args.length > 1){
+
+        verifierNaN([args[1], args[args.length -1]]);
+        let stat = parseInt(args[1]);
+        let modificateur = args.length > 2 ? `+ ${parseInt(args[args.length -1])}` : "";
+
+        if ( stat < 1 || stat > 20 ) {
+            throw("Stat incorrecte");
+        }
+
+        let lancer = ""
+
+        switch(stat){
+            case 1:
+                lancer = `2d4 ${modificateur}`;
+                break;
+            case 2:
+                lancer = `1d4+1d6 ${modificateur}`;
+                break;
+            case 3: case 4:
+                lancer = `2d6 ${modificateur}`;
+                break;
+            case 5: case 6:
+                lancer = `1d6+1d8 ${modificateur}`;
+                break;
+            case 7: case 8: case 9:
+                lancer = `2d8 ${modificateur}`;
+                break;
+            case 10: case 11: case 12:
+                lancer = `1d8+1d10 ${modificateur}`;
+                break;
+            case 13: case 14: case 15:
+                lancer = `2d10 ${modificateur}`;
+                break;
+            case 16: case 17: case 18: case 19:
+                lancer = `1d10+1d12 ${modificateur}`;
+                break;
+            case 20:
+                lancer = `2d12 ${modificateur}`;
+                break;
+        }
+        roll.roll(client, message, [lancer], envoyerPM, idMJ, `roll ${lancer}`);
+        return;
+    }
+
     outils.verifierNaN(args);
 
     let stat = args.length > 0 ? parseInt(args[0]) : 3; // Si aucune information est donnée, on assume que la stat est de 3 et le dd de 3. Si ça pose problème de toute façon le bot le mentionne.
@@ -31,18 +102,9 @@ exports.dng = function(client, message, args, envoyerPM, idMJ){
 
     let botReply = `${message.author.toString()} avec une stat de ${stat}, a lancé [${lancerCaracteristique}] [${lancerCritique}]. ${alerteStatLimite}`;
     outils.envoyerMessage(client, botReply, message, envoyerPM, idMJ);
-    outils.logLancer(message.author.username, `[${lancerCaracteristique}] [${lancerCritique}]`, `DNG ${stat}`);
+    outils.logLancer(message.author.username, `[${lancerCaracteristique}] [${lancerCritique}]`, `dng ${stat}`);
 
-    // Echelon Dé de puissance (Réussite de base de 10 ou 14 en fonction de si thème primaire ou secondaire)
-    // 1 2d4
-    // 2 1d4+1d6
-    // 3 2d6
-    // 5 1d6+1d8
-    // 7 2d8
-    // 10 1d8+1d10
-    // 13 2d10
-    // 16 1d10+1d12
-    // 20 2d12
+
 }
     //#region 
     /*
