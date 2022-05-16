986c420e42e448b03b81547192424e88eae68bb4
Buwaro-bots <superbenj@gmail.com>
2022-05-16 23:10:06 +0200

    Isekai : Retrait disable, remplacé par rerolls.

--- Commandes/isekai.js
+++ Commandes/isekai.js
@@ -8,51 +8,8 @@ let listeTags = ["Plante", "Poison", "DnG", "Base", "Starter", "Final", "Feu", "
 "Forme", "Alola", "Electrique", "Psy", "Sol", "Glace", "Acier", "Femelle", "Mâle", "Fée", "PasDnG", "Galar", "Combat", "Roche", "Hisui", "Nouveau", "Spectre", "Dragon",
 "Gen1", "Gen2", "Gen3", "Gen4", "Gen5", "Gen6", "Gen7", "Gen8", "Gen9", "Non-pokemon", "Digimon"]
 
-exports.isekai = function(client, message, args, envoyerPM, idMJ, messageReroll = null) {
+exports.isekai = function(client, message, args, envoyerPM, idMJ, messageReroll = null, timerSpoiler = 4500) {
 
-    if (args[0] === "disable") {
-        if (!disable.hasOwnProperty(message.author.id)) {
-            disable[message.author.id] = {"pokemons" : [], "tags" : []}; 
-        }
-        if (args[1] === "tags") {
-            for(let i = 2; i < args.length; i++) {
-                tagADisable = outils.rattrapageFauteOrthographe(listeTags, args[i]);
-                disable[message.author.id].tags.push(tagADisable);
-            }
-        }
-        else {
-            let listePokemon = [];
-            for (let i = 0; i < pokedex.length; i++) {
-                listePokemon.push(pokedex[i].nom);
-            }
-            for(let i = 1; i < args.length; i++) {
-                pokemonADisable = outils.rattrapageFauteOrthographe(listePokemon, args[i]);
-                if (disable[message.author.id].pokemons.length < 150) {
-                    disable[message.author.id].pokemons.push(pokemonADisable);
-                }
-                else {
-                    outils.envoyerMessage(client, botReply, "Vous avez déjà atteint la limite de pokemons à désactiver. Vous pouvez désactiver uniquement 150 pokemons à la fois.");
-                    break;
-                }
-            }
-        }
-
-        let botReply = `${message.author.toString()} Liste des pokémons disabled : `
-        for (let i = 0; i < disable[message.author.id].pokemons.length; i++) {
-            botReply += `${disable[message.author.id].pokemons[i]}, `;
-        }
-
-        botReply += "\nListe des tags disabled : ";
-       for (let i = 0; i < disable[message.author.id].tags.length; i++) {
-            botReply += `${disable[message.author.id].tags[i]}, `;
-        }
-
-        let writer = JSON.stringify(disable, null, 4); // On sauvegarde le fichier.
-        fs.writeFileSync('./Données/disable-isekai.json', writer);
-
-        outils.envoyerMessage(client, botReply, message);
-        return;
-    }
     /* En % le taux de forcer un nouveau pokémon, je conseille de mettre entre 1 et 5. 
     (pour Hisui, 3 jusqu'au 1er Mai, 2 jusqu'au 1er Juillet, puis 1 jusqu'à la 9G, puis retirer les tags nouveau sur les Hisui.) */
     let tauxDeNouveau = 2;
@@ -147,34 +104,15 @@ exports.tiragePokemon = function(listeTagsDemandes, idUtilisateur = null) {
             tagsEnvoye.push(outils.rattrapageFauteOrthographe(listeTags, listeTagsDemandes[i]));
         } 
 
-        // Etablissement de la disable list
-        let disablePokemons = [];
-        let disableTags = [];
-
-        if (idUtilisateur != null && disable.hasOwnProperty(idUtilisateur)) {
-            disablePokemons = disable[idUtilisateur].pokemons;
-            disableTags = disable[idUtilisateur].tags;
-        }
-
         let nouvelleListe = [];
         for (let i = 0; i < pokedex.length; i++) { // On fait une boucle sur tout le pokédex
-            if (!disablePokemons.includes(pokedex[i]["nom"])) { // Si le pokémon n'est pas disabled
-                let valide = true;
-                for (let j = 0; j < tagsEnvoye.length ; j++) { // On fait une deuxième boucle sur la liste des tags
-                    if (!pokedex[i]["tags"].includes(tagsEnvoye[j])) {
-                        valide = false; // Si le pokémon n'a pas l'un des tags, on ne l'incluera pas dans la liste
-                    }
-                }
-                for (let j = 0; j < disableTags.length ; j++) { // On fait une troisième boucle sur la liste des tags disabled
-                    if (pokedex[i]["tags"].includes(disableTags[j])) {
-                        valide = false; // Si le pokémon a l'un des tags, on ne l'incluera pas dans la liste
-                    }
-                }
-
-                if (valide) {
-                    nouvelleListe.push(pokedex[i]);
+            let valide = true;
+            for (let j = 0; j < tagsEnvoye.length ; j++) { // On fait une boucle sur la liste des tags
+                if (!pokedex[i]["tags"].includes(tagsEnvoye[j])) {
+                    valide = false; // Si le pokémon n'a pas l'un des tags, on ne l'incluera pas dans la liste
                 }
             }
+            if (valide) {nouvelleListe.push(pokedex[i]);}
         }
 
         if (nouvelleListe.length === 0) { // Si il n'y a pas de pokémon correspondant, on renvoit une erreur
