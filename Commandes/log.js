ccbec80f963e2a024670f4651289511bd709dda4
Buwaro-bots <superbenj@gmail.com>
2022-07-18 18:30:37 +0200

    Logs : Statistiques sur les lancers.

--- Commandes/log.js
+++ Commandes/log.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const outils = require("./outils.js");
 const config = require('../config.json');
+const binomialProbability = require('binomial-probability')
 
 exports.log = function(client, message, args, envoyerPM, idMJ) {
     // Si l'argument est effacer et que l'utilisateur est l'admin, on effectue une copie de sauvegarde en mettant la date du jour, puis on efface les logs
@@ -57,6 +58,12 @@ exports.log = function(client, message, args, envoyerPM, idMJ) {
 exports.listeJoueurs = function(nombreHeures, estCouleurs = false, canal = null, recupererPM = false) {
     let jets = JSON.parse(fs.readFileSync(__dirname + '/../Données/stats.json', 'utf-8'))
     let listeJoueurs = {};
+    let nombreD100Lancers = 0;
+    let nombreD100ReussitesCrit = 0;
+    let nombreD100EchecsCrit = 0;
+    let nombreDNGlancers = 0;
+    let DNGLancers = { 1 : [0,0], 2 : [0,0], 3 : [0,0], 4 : [0,0], 5 : [0,0], "crit" : 0}; // Un tableau est reussite / lancers
+
 
     for (let [key, value] of Object.entries(jets)) {
         if (estCouleurs) {
@@ -82,7 +89,27 @@ exports.listeJoueurs = function(nombreHeures, estCouleurs = false, canal = null,
                         texte += roll["type"] == "1d100" ? "\u001b[0;37m, " : ` (${roll["type"]})\u001b[0;37m, `;
                         listeLancers += texte;
                     }
+
+                    if (roll["type"] == "1d100") {
+                        nombreD100Lancers += 1;
+                        nombreD100ReussitesCrit += roll["lancer"] < 6 ? 1 : 0;
+                        nombreD100EchecsCrit += roll["lancer"] > 95 ? 1 : 0;
+                    }
+                    else if (roll["type"].includes("dng") && !(roll["lancer"].includes(",")) && !(roll["lancer"].includes("ini"))) {
+                        let lancer = roll["lancer"];
+                        lancer = lancer.replaceAll("[","");
+                        lancer = lancer.split("]");
+                        let stat = parseInt(roll["type"][4]);
+                        if (DNGLancers.hasOwnProperty(stat)) {
+                            nombreDNGlancers += 1;
+                            DNGLancers[stat][1] += 1;
+                            if (parseInt(lancer[0]) > 3) DNGLancers[stat][0] += 1;
+                            if (parseInt(lancer[1]) > 18) DNGLancers["crit"] += 1;
+                        }
+                    }
+
                 }
+
                 if (listeLancers.length > 950) {
                     let nombreCaracteres = Math.min(listeLancers.length-2, 998 - 20 - key.length);
                     listeLancers = listeLancers.slice(0,nombreCaracteres) + "\r\n\`\`\`";
@@ -91,13 +118,13 @@ exports.listeJoueurs = function(nombreHeures, estCouleurs = false, canal = null,
                     nomJoueur = `\`\`\`ansi\r\n\u001b[0;34m${key} \u001b[0;36m(partie ${numeroPartie})\u001b[0;37m`;
                     listeLancers = "";
                 }
-	    	}
-            if (listeLancers.length > 0) {
-                let nombreCaracteres = Math.min(listeLancers.length-2, 998);
-                listeLancers = listeLancers.slice(0,nombreCaracteres) + "\r\n\`\`\`";
-                listeJoueurs[nomJoueur] = listeLancers;
             }
-        }
+        if (listeLancers.length > 0) {
+            let nombreCaracteres = Math.min(listeLancers.length-2, 998);
+            listeLancers = listeLancers.slice(0,nombreCaracteres) + "\r\n\`\`\`";
+            listeJoueurs[nomJoueur] = listeLancers;
+        }}
+
         else {
             let nomJoueur = `${key}`;
             let listeLancers = [];
@@ -112,6 +139,12 @@ exports.listeJoueurs = function(nombreHeures, estCouleurs = false, canal = null,
                     let texte = ` ${etoiles}${roll["lancer"]}${etoiles}`;
                     texte += roll["type"] == "1d100" ? "" : ` (${roll["type"]})`;
                     listeLancers.push(texte);
+
+                    if (roll["type"] == "1d100") {
+                        nombreD100Lancers += 1;
+                        nombreD100ReussitesCrit += roll["lancer"] < 6 ? 1 : 0;
+                        nombreD100EchecsCrit += roll["lancer"] > 95 ? 1 : 0;
+                    }
                 }
             }
             if (listeLancers.length > 0) {
@@ -120,5 +153,26 @@ exports.listeJoueurs = function(nombreHeures, estCouleurs = false, canal = null,
         }
 	}
 
+    let nomJoueur = `\`\`\`ansi\r\n\u001b[0;34m~Statistiques\u001b[0;37m`
+    let listeLancers = "";
+    if (nombreD100Lancers > 7) {
+        listeLancers += `\r\nNombre de d100 : ${nombreD100Lancers}\r\n`;
+        listeLancers += `\u001b[0;32mNombre de réussites critiques : ${nombreD100ReussitesCrit} (Probabilité d'en faire au moins autant : ${Math.round( 100 - binomialProbability.cumulative(nombreD100Lancers, nombreD100ReussitesCrit -1, 0.05)*1000) / 10}%)\r\n`;
+        listeLancers += `\u001b[0;31mNombre d'échecs critiques : ${nombreD100EchecsCrit} (Probabilité d'en faire au moins autant : ${Math.round( 100 - binomialProbability.cumulative(nombreD100Lancers, nombreD100EchecsCrit -1, 0.05)*1000 ) / 10}%)\u001b[0;37m\r\n`;
+    }
+    if (nombreDNGlancers > 7) {
+        listeLancers += "\r\nStats pour DnG (nombre de 4+ sur les jets de caractéristique):\r\n"
+        if (DNGLancers[1][1] > 0) listeLancers += `\u001b[0;31mSur une stat de 1 : ${DNGLancers[1][0]}/${DNGLancers[1][1]} (${Math.round(DNGLancers[1][0]/DNGLancers[1][1] * 1000) / 10}% / 25%)\r\n`
+        if (DNGLancers[2][1] > 0) listeLancers += `\u001b[0;33mSur une stat de 2 : ${DNGLancers[2][0]}/${DNGLancers[2][1]} (${Math.round(DNGLancers[2][0]/DNGLancers[2][1] * 1000) / 10}% / 50%)\r\n`
+        if (DNGLancers[3][1] > 0) listeLancers += `\u001b[0;32mSur une stat de 3 : ${DNGLancers[3][0]}/${DNGLancers[3][1]} (${Math.round(DNGLancers[3][0]/DNGLancers[3][1] * 1000) / 10}% / 62%)\r\n`
+        if (DNGLancers[4][1] > 0) listeLancers += `\u001b[0;36mSur une stat de 4 : ${DNGLancers[4][0]}/${DNGLancers[4][1]} (${Math.round(DNGLancers[4][0]/DNGLancers[4][1] * 1000) / 10}% / 70%)\r\n`
+        if (DNGLancers[5][1] > 0) listeLancers += `\u001b[0;34mSur une stat de 5 : ${DNGLancers[5][0]}/${DNGLancers[5][1]} (${Math.round(DNGLancers[5][0]/DNGLancers[5][1] * 1000) / 10}% / 75%)\r\n`
+        listeLancers += `\u001b[0;35mNombre de critiques : ${DNGLancers["crit"]} / ${nombreDNGlancers} (${Math.round(DNGLancers["crit"] / nombreDNGlancers * 1000) / 10}% / 10%) \r\n`;
+    }
+
+    if (listeLancers.length > 0) {
+        listeLancers += "\`\`\`"
+        listeJoueurs[nomJoueur] = listeLancers;
+    }
     return listeJoueurs;
 }
