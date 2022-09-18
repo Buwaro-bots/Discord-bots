97c237041f665baa8f088c8fd5f791febd868ec0
Buwaro-bots <superbenj@gmail.com>
2022-09-18 15:01:57 +0200

    Musique : Diverses améliorations, mais bug quand lancé plusieurs fois.

--- Commandes/musique.js
+++ Commandes/musique.js
@@ -6,21 +6,36 @@ let listeServeurs = {};
 let listeChansons = require("./../données/musique.json");
 let listeUtilisateursGlobale = Object.keys(listeChansons);
 
+
 module.exports = {
     musique: function(client, message, args, envoyerPM, idMJ) {
-        if (message.author.id !== config.admin) return;
+        //if (message.author.id !== config.admin) return;
         if (args[0] === "stop") {
+            module.exports.verifierSiUtilisateurConnecté(message);
+            let serveur = listeServeurs[message.guildId];
+            let messageEnCours = serveur.message
+            if (messageEnCours !== null) {
+            let botReply = messageEnCours.content.replaceAll("\u001b[0;36m", "\u001b[0;30m");
+            botReply = botReply.replace("\u001b[1;35m", "\u001b[0;30m");
+            botReply = botReply.replace("\u001b[0;35m            Liste en cours", "\u001b[0;30m            Liste interrompue");
+            messageEnCours.edit(botReply);
+            }
+
             const connection = getVoiceConnection(message.guild.id)
+            // serveur.player.stop(true); // test
             connection.destroy();
             delete listeServeurs[message.guildId];
             return;
         }
-        if (args[0] === "skip") {
+        else if (args[0] === "skip") {
+            module.exports.verifierSiUtilisateurConnecté(message);
             let serveur = listeServeurs[message.guildId];
+            serveur.estSkip = true;
             serveur.player.stop();
             return;
         }
-        if (args[0] === "pause") {
+        else if (args[0] === "pause") {
+            module.exports.verifierSiUtilisateurConnecté(message);
             let serveur = listeServeurs[message.guildId];
             if (serveur.enPause) {
                 serveur.player.unpause();
@@ -34,11 +49,13 @@ module.exports = {
         
         const player = createAudioPlayer(); 
         
-        serveur = {
+        let serveur = {
             player : player,
-            enCours : true,
+            canal : message.member.voice.channel.id,
             enPause : false,
-            listeChansons : JSON.parse(JSON.stringify(listeChansons))
+            estSkip : false,
+            listeChansons : JSON.parse(JSON.stringify(listeChansons)),
+            message : null,
         };
         listeServeurs[message.guildId] = serveur;
 
@@ -60,12 +77,28 @@ module.exports = {
         });
         
         listeChansonsEnCours = [];
+        let messageEnCours = null;
+        let botReply;
+        let premièreOSTListe;
         player.on(AudioPlayerStatus.Idle, () => {
-            if (serveur.enCours === true) {
-                if (listeChansonsEnCours.length === 0) {
-                    let botReply = "```ansi\r\n";
-                    liste = message.guild.members.cache.filter(member => member.voice.channel);
-                    listeUtilisateursConnectés = Array.from(liste.keys());
+            if (listeChansonsEnCours.length === 0) {
+                if (messageEnCours !== null) {
+                    botReply = messageEnCours.content;
+                    botReply = botReply.replace("\u001b[0;35m            Liste en cours", "\u001b[0;30m            Liste terminée");
+                    if (serveur.estSkip) {
+                        botReply = botReply.replace("\u001b[1;35m", "\u001b[0;30m");
+                        serveur.estSkip = false;
+                    }
+                    else {
+                        botReply = botReply.replace("\u001b[1;35m", "\u001b[0;33m");
+                    }
+                    messageEnCours.edit(botReply);
+                }
+                botReply = "```ansi\r\n\u001b[0;35m            Liste en cours\r\n";
+                premièreOSTListe = true;
+                let liste = message.guild.members.cache.filter(member => member.voice.channel);
+                let listeUtilisateursConnectés = Array.from(liste.keys());
+                while (listeChansonsEnCours.length < 10){
                     for (let i = 1; i < listeUtilisateursGlobale.length; i++) {
                         let utilisateurEnCours = serveur.listeChansons[listeUtilisateursGlobale[i]];
                         
@@ -83,7 +116,7 @@ module.exports = {
                                 let numeroChansonARajouter = outils.randomNumber(utilisateurEnCours.liste.length) - 1;
                                 chansonARajouter = utilisateurEnCours.liste[numeroChansonARajouter];
                             }
-                            botReply += `\u001b[0;34m${utilisateurEnCours.nom} \u001b[0;36m${chansonARajouter.replace(/^.*[\\\/]/, '')}\r\n`;
+                            botReply += `\u001b[0;34m${utilisateurEnCours.nom}${"           ".slice(utilisateurEnCours.nom.length)} \u001b[0;36m${chansonARajouter.replace(/^.*[\\\/]/, '').slice(0,-4)}\r\n`;
                             listeChansonsEnCours.push(chansonARajouter);
                             for (let j = 1; j < listeUtilisateursGlobale.length; j++) {
                                 if (serveur.listeChansons[listeUtilisateursGlobale[j]].liste.includes(chansonARajouter)) {
@@ -92,18 +125,38 @@ module.exports = {
                             }
                         }
                     }
-                    botReply += "\u001b[0m```"
-                    outils.envoyerMessage(client, botReply, message);
                 }
-                let chansonAJouer = listeChansonsEnCours.shift();
-                resource = createAudioResource(chansonAJouer);
-                player.play(resource);
+                botReply += "\u001b[0m```";
+                botReply = botReply.replace("\u001b[0;36m", "\u001b[1;35m");
+                outils.envoyerMessage(client, botReply, message)
+                .then((msg)=> {
+                    messageEnCours = msg;
+                    serveur.message = msg;
+                });
             }
+            if (!premièreOSTListe) {
+                botReply = messageEnCours.content;
+                if (serveur.estSkip) {
+                    botReply = botReply.replace("\u001b[1;35m", "\u001b[0;30m");
+                    serveur.estSkip = false;
+                }
+                else {
+                    botReply = botReply.replace("\u001b[1;35m", "\u001b[0;33m");
+                }
+                botReply = botReply.replace("\u001b[0;36m", "\u001b[1;35m");
+                messageEnCours.edit(botReply);
+            }
+            premièreOSTListe = false;
+            let chansonAJouer = listeChansonsEnCours.shift();
+            resource = createAudioResource(chansonAJouer);
+            player.play(resource);
         });
     },
 
 
-    bot: function(client, message, args, envoyerPM, idMJ) {
-
+    verifierSiUtilisateurConnecté: function(message) {
+        if (message.member.voice.channel.id !== listeServeurs[message.guildId].canal) {
+            throw("Utilisateur non connecté au canal audio.")
+        }
     }
 }
